d68e1029960210e1269cc43f95203247faf67bec
yu-keun, OH <37852701+eottabom@users.noreply.github.com>
2026-02-11 21:42:58 +0900

    Update page title in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Eottabom's Lab. - 어느 따뜻한 봄날이 나에게도 오겠지... 공부하는 개발자 기술 블로그</title>
+        <title>Eottabom's Lab. - "어느 따뜻한 봄날이 나에게도 오겠지!!" - 공부하는 개발자 기술 블로그</title>
       </Head>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Component {...pageProps} />
