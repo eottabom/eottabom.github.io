16c4a4944fe5628ac75ac20c51547923f1ace64d
eottabom <eottabom@users.noreply.github.com>
2026-02-11 12:43:40 +0000

    deploy: d68e1029960210e1269cc43f95203247faf67bec

--- _next/static/chunks/27014fed3741aabc.js
+++ _next/static/chunks/27014fed3741aabc.js
@@ -1,7 +1,7 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/d13d92cbf88a7216.js",
+  "static/chunks/2b377f202d74570b.js",
   "static/chunks/11446f8a96c6cc02.js",
   "static/chunks/ece6820c01fea276.js",
   "static/chunks/f8803f7fba5ffefc.css",
-  "static/chunks/turbopack-83a1894e99a23919.js"
+  "static/chunks/turbopack-6ae21aa010af8624.js"
 ])
