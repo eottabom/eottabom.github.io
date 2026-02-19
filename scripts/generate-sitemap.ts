1304b10f5638cce9ef3b9b432becaf88b12c2d9d
eottabom <snoopy12oyk@gmail.com>
2026-02-19 20:48:38 +0900

    version up 1.0.2
    # Conflicts:
    #	version.properties

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -1,13 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 import { getAllPostIds } from '../lib/posts';
+import { getBooksMetaOnly } from '../lib/books';
 
 const BASE_URL = 'https://eottabom.github.io';
 
 const generateSitemap = () => {
     const posts = getAllPostIds();
+    const books = getBooksMetaOnly();
 
-    const urls = posts.map(({ params }) => `
+    const postUrls = posts.map(({ params }) => `
     <url>
       <loc>${BASE_URL}/post/${params.id}</loc>
       <changefreq>weekly</changefreq>
@@ -15,9 +17,18 @@ const generateSitemap = () => {
     </url>
   `).join('');
 
+    const bookUrls = books.map((book) => `
+    <url>
+      <loc>${BASE_URL}/book/${book.slug}</loc>
+      <changefreq>monthly</changefreq>
+      <priority>0.6</priority>
+    </url>
+  `).join('');
+
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls}
+    ${postUrls}
+    ${bookUrls}
   </urlset>`;
 
     fs.writeFileSync(path.join('public', 'sitemap.xml'), sitemap.trim());
