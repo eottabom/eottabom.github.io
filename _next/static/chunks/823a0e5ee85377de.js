5d72e5ce974ea54cfacd51259b8514a9d2062956
eottabom <eottabom@users.noreply.github.com>
2026-03-05 12:53:28 +0000

    deploy: 9e8c91e229d79cc9ccf198ec9c429ccbc9219511

--- _next/static/chunks/823a0e5ee85377de.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/post/[id]", [
-  "static/chunks/fe95dff0a8c84727.js",
-  "static/chunks/29f6d77648d106e5.js",
-  "static/chunks/ece6820c01fea276.js",
-  "static/chunks/11446f8a96c6cc02.js",
-  "static/chunks/turbopack-b3521842b2840946.js"
-])
--- _next/static/chunks/b836153c9caed7fe.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/book/[slug]", [
-  "static/chunks/7494f5b528fb1aac.js",
-  "static/chunks/29f6d77648d106e5.js",
-  "static/chunks/11446f8a96c6cc02.js",
-  "static/chunks/ece6820c01fea276.js",
-  "static/chunks/turbopack-c30b42853bccd4ce.js"
-])
