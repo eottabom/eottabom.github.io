fa4034634deac0b4aeda4dcf4984133b20cb26c8
eottabom <eottabom@users.noreply.github.com>
2026-09-04 13:28:21 +0000

    deploy: 1bacea2b4b6e3f5c04eea20c60a5b349863c8bf0

--- _next/static/chunks/067xc21gz06dp.js
+++ _next/static/chunks/067xc21gz06dp.js
@@ -3,6 +3,6 @@ __turbopack_load_page_chunks__("/post/[id]", [
   "static/chunks/14k9rj2e9opxa.js",
   "static/chunks/30t2ftcy36gjm.js",
   "static/chunks/363tip92cw93q.js",
-  "static/chunks/0yc4w9vf0mg2w.js",
-  "static/chunks/turbopack-3as5yua1ti__z.js"
+  "static/chunks/1twre47n7r_ud.js",
+  "static/chunks/turbopack-41fwp2vies6u9.js"
 ])
